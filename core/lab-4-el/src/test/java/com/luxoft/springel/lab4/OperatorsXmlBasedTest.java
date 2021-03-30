2fd1960a8188c48e14697046d5e8fd562ad213a5
shchienko <shchienko@haulmont.com>
2021-03-31 01:35:05 +0400

    Lab 4 El

--- core/lab-4-el/src/test/java/com/luxoft/springel/lab4/OperatorsXmlBasedTest.java
+++ core/lab-4-el/src/test/java/com/luxoft/springel/lab4/OperatorsXmlBasedTest.java
@@ -1,38 +1,39 @@
 package com.luxoft.springel.lab4;
 
-import static org.junit.Assert.*;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class OperatorsXmlBasedTest {
-	
-	private static final String APPLICATION_CONTEXT_XML_FILE_NAME = "application-context.xml";
-	private AbstractApplicationContext context;
-	
-	@Before
-	public void setUp() throws Exception {
-		context = new ClassPathXmlApplicationContext(APPLICATION_CONTEXT_XML_FILE_NAME);
-	}
-	
-	@Test
-	public void testLessThen() {
-		Operators op = (Operators) context.getBean("operatorsBean");
-		assertTrue(op.isLessThanTest());
-	}
 
-	@Test
-	public void testAdd() {
-		Operators op = (Operators) context.getBean("operatorsBean");
-		assertEquals(op.getAddTest(), 14000000);
-	}
+    private static final String APPLICATION_CONTEXT_XML_FILE_NAME = "application-context.xml";
+    private AbstractApplicationContext context;
 
-	@After
-	public void tearDown() {
-		context.close();
-	}
+    @Before
+    public void setUp() {
+        context = new ClassPathXmlApplicationContext(APPLICATION_CONTEXT_XML_FILE_NAME);
+    }
+
+    @Test
+    public void testLessThen() {
+        Operators op = context.getBean("operatorsBean", Operators.class);
+        assertTrue(op.isLessThanTest());
+    }
+
+    @Test
+    public void testAdd() {
+        Operators op = context.getBean("operatorsBean", Operators.class);
+        assertEquals(op.getAddTest(), 14000000);
+    }
+
+    @After
+    public void tearDown() {
+        context.close();
+    }
 
 }
