8700b16c54a517cb89d8a33f51f6bf2b84a9cdc8
shchienko <shchienko@haulmont.com>
2021-03-31 01:18:05 +0400

    Lab 2 Core

--- core/lab-2-core/src/test/java/com/luxoft/springioc/lab2/model/SimpleAppTest.java
+++ core/lab-2-core/src/test/java/com/luxoft/springioc/lab2/model/SimpleAppTest.java
@@ -1,60 +1,61 @@
 package com.luxoft.springioc.lab2.model;
-import static org.junit.Assert.*;
 
-import org.junit.*;
+import org.junit.Before;
+import org.junit.Test;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.luxoft.springioc.lab2.model.Country;
-import com.luxoft.springioc.lab2.model.UsualPerson;
-
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class SimpleAppTest {
-	
-	protected static final String APPLICATION_CONTEXT_XML_FILE_NAME = "classpath:application-context.xml";
 
-	private AbstractApplicationContext context;
+    protected static final String APPLICATION_CONTEXT_XML_FILE_NAME = "classpath:application-context.xml";
 
-	private UsualPerson expectedPerson;
+    public static final String PERSON = "person";
 
-	@Before
-	public void setUp() throws Exception {
-		context = new ClassPathXmlApplicationContext(
-				APPLICATION_CONTEXT_XML_FILE_NAME);
-		expectedPerson = getExpectedPerson();
-	}
+    private AbstractApplicationContext context;
 
-	@Test
-	public void testInitPerson() {
-		UsualPerson person = (UsualPerson) context.getBean("person");
-//		FYI: Another way to achieve the bean
+    private UsualPerson expectedPerson;
+
+    @Before
+    public void setUp() {
+        context = new ClassPathXmlApplicationContext(
+                APPLICATION_CONTEXT_XML_FILE_NAME);
+        expectedPerson = getExpectedPerson();
+    }
+
+    @Test
+    public void testInitPerson() {
+        UsualPerson person = context.getBean(PERSON, UsualPerson.class);
+//		or
 //		person = context.getBean(UsualPerson.class);
-		assertEquals(expectedPerson, person);
-		System.out.println(person);
-	}
+        assertEquals(expectedPerson, person);
+        System.out.println(person);
+    }
 
-	private UsualPerson getExpectedPerson() {
-		UsualPerson person = new UsualPerson();
-		person.setAge(35);
-		person.setHeight(1.78F);
-		person.setIsProgrammer(true);
-		person.setName("Ivan Ivanov");
+    private UsualPerson getExpectedPerson() {
+        UsualPerson person = new UsualPerson();
+        person.setAge(35);
+        person.setHeight(1.78F);
+        person.setIsProgrammer(true);
+        person.setName("Ivan Ivanov");
 
-		Country country = new Country();
-		country.setId(1);
-		country.setName("Russia");
-		country.setCodeName("RU");
+        Country country = new Country();
+        country.setId(1);
+        country.setName("Russia");
+        country.setCodeName("RU");
 
-		person.setCountry(country);
+        person.setCountry(country);
 
-		List<String> contacts = new ArrayList<String>();
-		contacts.add("asd@asd.ru");
-		contacts.add("+7-234-456-67-89");
+        List<String> contacts = new ArrayList<String>();
+        contacts.add("asd@asd.ru");
+        contacts.add("+7-234-456-67-89");
 
-		person.setContacts(contacts);
+        person.setContacts(contacts);
 
-		return person;
-	}
+        return person;
+    }
 }
--- core/lab-2-core/src/test/java/com/luxoft/springioc/lab2/model/SpringTCFAppTest.java
+++ core/lab-2-core/src/test/java/com/luxoft/springioc/lab2/model/SpringTCFAppTest.java
@@ -1,8 +1,4 @@
 package com.luxoft.springioc.lab2.model;
-import static org.junit.Assert.assertEquals;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -11,51 +7,53 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.luxoft.springioc.lab2.model.Country;
-import com.luxoft.springioc.lab2.model.UsualPerson;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:application-context.xml")
 public class SpringTCFAppTest {
-	
-	@Autowired
-	private UsualPerson person;
 
-	private UsualPerson expectedPerson;
-	
+    @Autowired
+    private UsualPerson person;
 
-	@Before
-	public void setUp() throws Exception {
-		expectedPerson = getExpectedPerson();
-	}
+    private UsualPerson expectedPerson;
 
-	@Test
-	public void testInitPerson() {
-		assertEquals(expectedPerson, person);
-		System.out.println(person);
-	}
 
-	private UsualPerson getExpectedPerson() {
-		UsualPerson person = new UsualPerson();
-		person.setAge(35);
-		person.setHeight(1.78F);
-		person.setIsProgrammer(true);
-		person.setName("Ivan Ivanov");
+    @Before
+    public void setUp() {
+        expectedPerson = getExpectedPerson();
+    }
 
-		Country country = new Country();
-		country.setId(1);
-		country.setName("Russia");
-		country.setCodeName("RU");
+    @Test
+    public void testInitPerson() {
+        assertEquals(expectedPerson, person);
+        System.out.println(person);
+    }
 
-		person.setCountry(country);
+    private UsualPerson getExpectedPerson() {
+        UsualPerson person = new UsualPerson();
+        person.setAge(35);
+        person.setHeight(1.78F);
+        person.setIsProgrammer(true);
+        person.setName("Ivan Ivanov");
 
-		List<String> contacts = new ArrayList<String>();
-		contacts.add("asd@asd.ru");
-		contacts.add("+7-234-456-67-89");
+        Country country = new Country();
+        country.setId(1);
+        country.setName("Russia");
+        country.setCodeName("RU");
 
-		person.setContacts(contacts);
+        person.setCountry(country);
 
-		return person;
-	}
+        List<String> contacts = new ArrayList<>();
+        contacts.add("asd@asd.ru");
+        contacts.add("+7-234-456-67-89");
+
+        person.setContacts(contacts);
+
+        return person;
+    }
 
 }
